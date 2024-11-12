15a2b5060534d8671ca0196219e0d84388dc88a6
wrzyorii <ethanbatumbakal04@gmail.com>
2024-11-12 17:13:04 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something'.split('')
+const text = 'Happy 5th Monthsary!'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
